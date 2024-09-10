fea4d19d1537570dd47042d16d1a89dee92f41ca
xiaozh <rafftale@gmail.com>
2024-09-10 23:24:28 +0800

    controller-routing

---
+++ src/cats.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(request.body);
+    return 'This action returns all cats';
+  }
+}
