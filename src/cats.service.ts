f1b3989bba7a7d4b27eadc85925ae0629305b60d
xiaozh <rafftale@gmail.com>
2024-09-12 21:33:29 +0800

    http

--- src/cats.service.ts
+++ src/cats.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class CatsService {
   private readonly cats: Cat[] = [];
+  private readonly logger = new Logger(CatsService.name);
+  constructor(private readonly httpService: HttpService) {}
 
   create(cat: Cat) {
     this.cats.push(cat);
@@ -11,5 +16,17 @@ export class CatsService {
 
   findAll(): Cat[] {
     return this.cats;
+  }
+
+  async listAll(): Promise<Cat[]> {
+    const { data } = await firstValueFrom(
+      this.httpService.get<Cat[]>('http://localhost:3000/cats').pipe(
+        catchError((error: AxiosError) => {
+          this.logger.error(error.response.data);
+          throw 'An error happened!';
+        }),
+      ),
+    );
+    return data;
   }
 }
